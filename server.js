4b647b38a8b111e646fccc77a8c5e73274a83e70
Chin Yan Keat (Digi - Central) <YKCHIN@DiGiCR.digi.com.my>
2017-04-10 19:39:35 +0800

    added shortcut2

--- server.js
+++ server.js
@@ -8,7 +8,7 @@
 //
 ////////////////////////////////////////////////////////////
 
-require('dotenv-extended').load();
+//require('dotenv-extended').load();
 
 var restify = require('restify');
 var builder = require('botbuilder');
