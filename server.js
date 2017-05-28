756aca2cd3fa998d29017dd3a664c05c1fbe2331
Chin Yan Keat (Digi - Central) <YKCHIN@DiGiCR.digi.com.my>
2017-05-28 11:24:56 +0800

    add back NLP

--- server.js
+++ server.js
@@ -2210,33 +2210,33 @@ function findStackAction(routes, name) {
 bot.dialog('CatchAll', [
     function (session) {
 
-//console.log("text: "+session.message.text + apiai_app);
-//		var request = apiai_app.textRequest(session.message.text, {
-//			sessionId: `${math.randomInt(100000,999999)}`
-//		});
-//
-//		request.on('response', function(response) {
-//			if(response.result.action==undefined){
-//				session.send("Let's get back to our chat on Digi");
-//			} else {		// We have response from API.AI
-//				console.log("API.AI [" +response.result.resolvedQuery + '][' + response.result.action + '][' + response.result.score + ']['  + response.result.fulfillment.speech + ']');
-//	//			console.log('API.AI response text:'+ response.result.fulfillment.speech);
-//	//			console.log('API.AI response text:'+ response.result.fulfillment.messages[0].speech);
-//	//			console.log('API.AI response:'+ JSON.stringify(response.result));
-//				if(response.result.fulfillment.speech.length>0) {
-//					session.send(response.result.fulfillment.speech);				
-//				} else {
-//					session.send("Let's get back to our chat on Digi");
-//				}
-//			}
-//		});
-//
-//		request.on('error', function(error) {
-//			console.log('API.AI error:'+error);
-			session.send("Let's get back to our chat on Digi");
-//		});
+//		console.log("text: "+session.message.text + apiai_app);
+		var request = apiai_app.textRequest(session.message.text, {
+			sessionId: `${math.randomInt(100000,999999)}`
+		});
 
-//		request.end();
+		request.on('response', function(response) {
+			if(response.result.action==undefined){
+				session.send("Let's get back to our chat on Digi");
+			} else {		// We have response from API.AI
+				console.log("API.AI [" +response.result.resolvedQuery + '][' + response.result.action + '][' + response.result.score + ']['  + response.result.fulfillment.speech + ']');
+//				console.log('API.AI response text:'+ response.result.fulfillment.speech);
+//				console.log('API.AI response text:'+ response.result.fulfillment.messages[0].speech);
+//				console.log('API.AI response:'+ JSON.stringify(response.result));
+				if(response.result.fulfillment.speech.length>0) {
+					session.send(response.result.fulfillment.speech);				
+				} else {
+					session.send("Let's get back to our chat on Digi");
+				}
+			}
+		});
+
+		request.on('error', function(error) {
+			console.log('API.AI error:'+error);
+			session.send("Let's get back to our chat on Digi");
+		});
+
+		request.end();
 	}
 ]).triggerAction({
     matches: /^.*$/i
